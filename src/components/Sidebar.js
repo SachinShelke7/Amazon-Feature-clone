4debefb29ad3c7566895376729885b5d3abbd884
Sachin Shelke <shelkesp1@gmail.com>
2022-01-10 10:29:39 +0530

    small screen fix

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -5,18 +5,18 @@ import { Link } from "react-router-dom";
 function Sidebar({categoryList}) {
   const [showSidebar, setShowSidebar] = useState(false);
   return (
-    <div className="flex justify-center items-center flex-col min-h-screen py-2 w-full md:hidden">
+    <div className="md:hidden">
       {showSidebar ? (
         <p
           onClick={() => setShowSidebar(!showSidebar)}
-          className="flex text-white items-center cursor-pointer fixed right-4 top-[62px] z-50"
+          className="flex text-gray-300 hover:text-white items-center cursor-pointer fixed right-6 top-[62px] z-50 hover:scale-110"
         >
-          X
+           Close
         </p>
       ) : (
         <p
           onClick={() => setShowSidebar(!showSidebar)}
-          className="flex text-white items-center cursor-pointer fixed right-0 pr-2 top-[62px] z-30"
+          className="flex text-gray-300 hover:text-white items-center cursor-pointer fixed right-0 pr-2 top-[62px] z-30 "
         >
           CATEGORIES
           <MdOutlineMenu className="pl-2 w-8 h-8"/>
@@ -24,16 +24,19 @@ function Sidebar({categoryList}) {
         
       )}
 
-      <div className={`top-14 right-0 w-full lg:w-[32vw] bg-[#232f3e] p-10 pl-20 text-white fixed h-full z-40 ease-in-out duration-500 ${ showSidebar ? "translate-x-0":"translate-x-full"}`}>
+      <div className={`top-14 right-0 w-full lg:w-[32vw] bg-[#232f3e] pt-14 md:p-10 md:pl-20 text-white fixed h-full z-40 ease-in-out duration-500 ${ showSidebar ? "translate-x-0":"translate-x-full"}`}>
         <ul className="flex flex-col items-center space-y-5">
-          {/* <li className="flex items-center">
+        <Link to={`/`} onClick={() => setShowSidebar(!showSidebar)}>
+           <li className="flex items-center text-gray-300 font-medium hover:scale-105 hover:text-white">
             <MdOutlineMenu />
-            All
-          </li> */}
+            Home
+          </li> 
+                </Link>
+
                     {
             categoryList?.map(category=>{
               return <li key={category.id} className="text-gray-300 font-medium hover:scale-105 hover:text-white">
-                <Link to={`/category/${category.slug}`}>
+                <Link to={`/category/${category.slug}`} onClick={() => setShowSidebar(!showSidebar)}>
                 {category.name}
                 </Link>
 
